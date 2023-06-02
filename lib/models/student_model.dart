1724c87ffea725b7c9c56534ef0440c277033244
abdullahabdulrehman1 <abdullahabdurrehman72@gmail.com>
2023-06-02 21:34:03 +0500

    a

--- lib/models/student_model.dart
+++ lib/models/student_model.dart
@@ -1,15 +1,17 @@
 class StudentModel {
-  final Name;
-  final Email;
-  final Addresss;
-  final id;
-  StudentModel({this.Name, this.Addresss, this.Email, this.id});
+  final String Name;
+  final String Email;
+  final String Addresss;
+  final int id;
+
+  StudentModel({required this.Name, required this.Addresss, required this.Email, required this.id});
 
   factory StudentModel.fromJson(Map<String, dynamic> json) {
     return StudentModel(
-        Name: json['Name'],
-        Addresss: json['Address'],
-        Email: json['Email'],
-        id: json['id']);
+      Name: json['Name'],
+      Addresss: json['Address'],
+      Email: json['Email'],
+      id: json['id'],
+    );
   }
 }
